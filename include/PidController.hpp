b0ced396bde924678cb1d45c3f989eddf501bc0f
roboticistjoseph <jkatak73@terpmail.umd.edu>
2022-09-30 21:54:35 -0400

    Class Declaration for PID Controller

---
+++ include/PidController.hpp
@@ -0,0 +1,78 @@
+/**
+ * @file PidController.hpp
+ * @author Driver: Joseph Pranadeer Reddy Katakam (jkatak73@umd.edu)
+ * @author Navigator: Adithya Gaurav Singh (agsingh@umd.edu)
+ * @brief Class declaration for PidController
+ * @version 0.1
+ * @date 2022-09-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+#include <iostream>
+
+/**
+ * @brief Class for PID Controller
+ * 
+ */
+class PidController {
+    private:
+        /**
+         * @brief Proportional Gain
+         * 
+         */
+        double k_p;
+
+        /**
+         * @brief Integral Gain
+         * 
+         */
+        double k_i;
+
+        /**
+         * @brief Derivative Gain
+         * 
+         */
+        double k_d;
+
+        /**
+         * @brief Time Interval
+         * 
+         */
+        double d_t;
+    
+    public:
+        /**
+        * @brief Default Constructor for Pid Controller Class
+        * 
+        */
+        PidController();
+
+        /**
+         * @brief Custom Constructor for Pid Controller Class
+         * 
+         * @param proportional_constant 
+         * @param integration_constant 
+         * @param derivative_constant 
+         * @param time_interval 
+         */
+        PidController(double proportional_constant, double integration_constant, double derivative_constant, double time_interval);
+        
+        /**
+         * @brief Compute PID controller output
+         * 
+         * @param target_velocity 
+         * @param current_velocity 
+         * @return double 
+         */
+        double compute(double target_velocity, double current_velocity);
+
+        /**
+         * @brief Destructor for Pid Controller Class
+         * 
+         */
+        ~PidController();
+
+};
